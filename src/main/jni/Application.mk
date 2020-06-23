7e6c66bf219f6db37e08313113c17211bf4ff14f
hongzhaji <pltyhzj@163.com>
2020-06-23 16:25:44 +0800

    android file and jni

---
+++ src/main/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_PLATFORM := android-16
+APP_ABI := armeabi-v7a
+APP_OPTIM := release
+APP_STL := c++_static
+APP_CPPFLAGS := -frtti -fexceptions
---
+++ src/main/obj/local/armeabi-v7a/objs/NativeCameraPlugin/src/RenderingPlugin.o.d
@@ -0,0 +1,11 @@
+G:/AndroidProject/CameraToUnity/cameraplugin/src/main/obj/local/armeabi-v7a/objs/NativeCameraPlugin/src/RenderingPlugin.o: \
+  G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/RenderingPlugin.cpp \
+  G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/RenderingPlugin.h \
+  G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/Unity/IUnityInterface.h \
+  G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/Unity/IUnityGraphics.h
+
+G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/RenderingPlugin.h:
+
+G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/Unity/IUnityInterface.h:
+
+G:/AndroidProject/CameraToUnity/cameraplugin/src/main/jni/src/Unity/IUnityGraphics.h:
